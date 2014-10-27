db7dfffd6f767e108f8420e307adab64f27492e3
ivinke <ivinke@chicagobooth.edu>
2014-10-27 13:05:12 -0500

    Finished API Project

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -27,28 +27,19 @@ puts the_latitude
 the_longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 puts the_longitude
 
-
 puts "The latitude of #{the_address} is #{the_latitude} and the longitude is #{the_longitude}."
 
-the_weather=gets.chomp
-url_safe_address = URI.encode(the_address)
-
-url_of_weather_we_want = "https://api.forecast.io/forecast/cc9e23dfdaa96dd75a1e215f3abe04cd/" + the_weather
-puts url_of_weather_we_want
-
-puts url_of_weather_we_want
+url_of_weather_we_want = "https://api.forecast.io/forecast/cc9e23dfdaa96dd75a1e215f3abe04cd/#{the_latitude},#{the_longitude}"
 
 raw_data1 = open(url_of_weather_we_want).read
 parsed_data1 = JSON.parse(raw_data1)
-puts parsed_data1
 
-the_address
-the_temperature
-
-
-
-# Ultimately, we want the following line to work when uncommented:
+the_temperature = parsed_data1["currently"]["temperature"]
+the_hour_outlook = parsed_data1["hourly"]["summary"]
+the_day_outlook = parsed_data1["daily"]["summary"]
 
 puts "The current temperature at #{the_address} is #{the_temperature} degrees."
 puts "The outlook for the next hour is: #{the_hour_outlook}"
 puts "The outlook for the next day is: #{the_day_outlook}"
+
+
